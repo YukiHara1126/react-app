9c7ae23678a97483e607d2bc03f7e0ccc59c0704
Yuki Hara <yuki.hara1126@gmail.com>
2022-06-09 08:29:08 +0900

    add fastApi

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
 import AppFooter from '../AppFooter';
 import { Button, ChakraProvider } from '@chakra-ui/react'
+import axios from "axios"
 
 const App = () => {
+
+    const [data, setData] = React.useState();
+    const url = "http://127.0.0.1:8000"
+
+    const GetData = () => {
+        axios.get(url).then((res) => {
+            setData(res.data);
+        })
+    }
     return (
         <ChakraProvider>
             <h1>お元気ですか</h1>
             <p>お元気ですか</p>
-            <Button colorScheme="teal">ボタン</Button>
+            {data ? <div>{data.Hello}</div> : <Button colorScheme="teal" onClick={GetData}>データを取得</ Button>}
+
             <AppFooter />
         </ChakraProvider>
     );
